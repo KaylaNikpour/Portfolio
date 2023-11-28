2dbf4a84c342905e01de3f5206a3ea8997671e54
KaylaNikpour <kayla.nikpour@edu.sait.ca>
2023-11-28 10:26:10 -0700

    button

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 console.log('Hello, World!');
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+function onButtonClick() {
+    alert('If you like Twilight too then we can be friends!');
+  }
+  
+  const button = document.querySelector('button');
+  button.addEventListener('click', onButtonClick);
+  
+  const newButton = document.createElement('button');
+  newButton.textContent = 'Click me!';
+  document.body.appendChild(newButton);
+  
+  newButton.addEventListener('click', () => {
+    alert('New button clicked!');
+  });
+
+ 
+      
