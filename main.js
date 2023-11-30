c0e58b1e5427848903e42ae93cd2db6c008a718f
KaylaNikpour <kayla.nikpour@edu.sait.ca>
2023-11-30 15:14:46 -0700

    Fixed my button ID using  #

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log('Hello, World!');
-
+// Copyright year
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-
+//button click 
 function onButtonClick() {
     alert('I hope you are having the most beautiful day :)');
   }
@@ -10,19 +10,21 @@ function onButtonClick() {
   const button = document.querySelector('button');
   button.addEventListener('click', onButtonClick);
 
-// Ad a mouseover event listener
+// Button hover
 button.addEventListener('mouseover', () => {
-  // Change the button's background color
   button.style.text = 'Click me!';
   button.style.backgroundColor = 'blue';
   changeText.textContent = 'Click me!';
 });
 
-// Add a mouseout event listener
 button.addEventListener('mouseout', () => {
-  // Change the button's background color back to its original color
   button.style.backgroundColor = '';
   changeText.textContent = 'Hover over me!'
 });
 const changeText = document.querySelector("button");
  
+let count = 1;
+document.getElementById('btn-counter').onclick = () => {
+     count = count + 1;
+     document.getElementById('btn-counter').innerHTML = 'message'
+}
